6dca64f5e5b561583412e5af613d16d281d226c5
Kai Janicek <dasunwichtige@gmail.com>
2024-04-01 19:08:09 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let currentPokemon;
+
+async function loadPokemon() {
+    let url = 'https://pokeapi.co/api/v2/pokemon/charmander';
+    let response = await fetch(url);
+    currentPokemon = await response.json();
+    console.log(currentPokemon);
+    renderPokemonInfo();
+
+}
+
+
+function renderPokemonInfo() {
+    document.getElementById('pokemonName').innerHTML = currentPokemon['name'];
+    document.getElementById('pokemonImage').src = currentPokemon['sprites']['front_shiny'];
+}
