d95e0d72b5e9593f6bbe263980c0cf09a226e4cd
Kai Janicek <dasunwichtige@gmail.com>
2024-04-03 19:09:58 +0200

    show information in chart

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let currentPokemon;
+let stats = [];
+let myChart;
 
 async function init() {
   await loadPokemon();
-  renderPokemonInfo()
+  renderPokemonInfo();
 }
 
 async function loadPokemon() {
@@ -14,41 +16,68 @@ async function loadPokemon() {
 
 function renderPokemonInfo() {
   document.getElementById("pokemonName").innerHTML = currentPokemon["name"];
-  document.getElementById("pokemonImage").src = currentPokemon["sprites"]["front_shiny"];
-  document.getElementById("species").innerHTML = currentPokemon["types"][0]["type"]["name"];
-  document.getElementById("typeCard").innerHTML = currentPokemon["types"][0]["type"]["name"];
-  document.getElementById("height").innerHTML = currentPokemon["height"] + " cm";
-  document.getElementById("weight").innerHTML = currentPokemon["weight"] + " kg";
-  document.getElementById("abilities").innerHTML = currentPokemon["abilities"][0]["ability"]["name"];
+  document.getElementById("pokemonImage").src =
+    currentPokemon["sprites"]["front_shiny"];
+  document.getElementById("species").innerHTML =
+    currentPokemon["types"][0]["type"]["name"];
+  document.getElementById("typeCard").innerHTML =
+    currentPokemon["types"][0]["type"]["name"];
+  document.getElementById("height").innerHTML =
+    currentPokemon["height"] + " cm";
+  document.getElementById("weight").innerHTML =
+    currentPokemon["weight"] + " kg";
+  document.getElementById("abilities").innerHTML =
+    currentPokemon["abilities"][0]["ability"]["name"];
 }
 
 async function renderPokemonCard() {
   await loadPokemon();
   document.getElementById("pokemonName").innerHTML = currentPokemon["name"];
-  document.getElementById("pokemonImage").src = currentPokemon["sprites"]["front_shiny"];
-  document.getElementById("typeCard").innerHTML = currentPokemon["types"][0]["type"]["name"];
-  renderChart();
+  document.getElementById("pokemonImage").src =
+    currentPokemon["sprites"]["front_shiny"];
+  document.getElementById("typeCard").innerHTML =
+    currentPokemon["types"][0]["type"]["name"];
+  renderStatsInCharts();
 }
 
 function renderChart() {
-const ctx = document.getElementById('myChart');
+  const ctx = document.getElementById("myChart");
 
-  new Chart(ctx, {
-    type: 'bar',
+  // Überprüfe, ob ein Chart bereits existiert, und zerstöre es
+  if (myChart) {
+    myChart.destroy();
+  }
+
+  // Erstelle das neue Chart-Objekt und speichere es in der Variable myChart
+  myChart = new Chart(ctx, {
+    type: "bar",
     data: {
-      labels: ['HP', 'Attack', 'Defense', 'Sp. Atk', 'Sp. Def', 'Speed', 'Total'],
-      datasets: [{
-        label: 'Stats',
-        data: [12, 19, 3, 5, 2, 3, 37],
-        borderWidth: 1
-      }]
+      labels: ["HP", "Attack", "Defense", "Sp. Atk", "Sp. Def", "Speed"],
+      datasets: [
+        {
+          label: "Stats",
+          data: stats,
+          borderWidth: 1,
+        },
+      ],
     },
     options: {
       scales: {
         y: {
-          beginAtZero: true
-        }
-      }
-    }
+          beginAtZero: true,
+        },
+      },
+    },
   });
+}
+
+function renderStatsInCharts() {
+  stats.push(currentPokemon["stats"][0]["base_stat"]);
+  stats.push(currentPokemon["stats"][1]["base_stat"]);
+  stats.push(currentPokemon["stats"][2]["base_stat"]);
+  stats.push(currentPokemon["stats"][3]["base_stat"]);
+  stats.push(currentPokemon["stats"][4]["base_stat"]);
+  stats.push(currentPokemon["stats"][5]["base_stat"]);
+  console.log(stats);
+  renderChart();
 }
